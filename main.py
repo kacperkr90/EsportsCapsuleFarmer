29d1169328172106b900301c5aa813cdd75bd072
kacperkr90 <odi125@protonmail.com>
2022-09-08 09:47:09 +0200

    Remove password from logs in containerized config

--- main.py
+++ main.py
@@ -242,9 +242,10 @@ if containerised:
     multiplier = to_int_or_default(os.environ.get('WAIT_VALUES_MULTIPLIER'), 1)
     remoteWdHubUrl = os.environ.get('REMOTE_WD_HUB_URL')
     delay = to_int_or_default(os.environ.get('DELAY_IN_SECONDS'), delay)
-    log.info('Config values: [ headless=%s, autologin=%s, browser=%s, username=%s, password=%s, '
-             'wait_values_multiplier=%s, remoteWdHubUrl=%s, delay=%s]', isHeadless, hasAutoLogin, browser, username,
-             password, multiplier, remoteWdHubUrl, delay)
+    log.info('Config values: [ headless=%s, autologin=%s, browser=%s, '
+             'username=%s, password=*********, wait_values_multiplier=%s, '
+             'remoteWdHubUrl=%s, delay=%s]', isHeadless, hasAutoLogin, browser,
+             username, multiplier, remoteWdHubUrl, delay)
 
 if not (isHeadless and hasAutoLogin):
     log.info("Consider using the headless mode for improved performance and stability.")
